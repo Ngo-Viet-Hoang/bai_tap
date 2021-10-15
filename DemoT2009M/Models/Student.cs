26363aaf59be64da0fa3bc29717462562bf0a66d
Ngo-Viet-Hoang <ngoviethoang007@gmail.com>
2021-10-15 18:39:09 +0700

    bai tap

---
+++ DemoT2009M/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace DemoT2009M.Models
+{
+    public class Student
+    {
+        private string rollNumber;
+
+        public string GetRollNumber()
+        {
+            if (string.IsNullOrEmpty(rollNumber))
+            {
+                return "RollNumber has no value";
+            }
+
+            return rollNumber;
+        }
+
+        public void SetRollNumber(string rollNumber)
+        {
+            if (string.IsNullOrEmpty(rollNumber))
+            {
+                return;
+            }
+
+            this.rollNumber = rollNumber;
+        }
+    }
+}
---
+++ DemoT2009M/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoT2009M
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student sinhvien = new Student();
+            sinhvien.s = "A001";
+        }
+    }
+}
